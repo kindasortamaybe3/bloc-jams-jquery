c7bc4d1ee8dd8e4fba44745e34c6ade973b1a040
Kara Bruce <kindasortamaybe3@gmail.com>
2019-12-01 11:22:09 +0100

    added more comments

--- scripts/player.js
+++ scripts/player.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor () {
+  constructor() {
     this.currentlyPlaying = album.songs[0];
     this.playState = 'stopped';
     this.volume = 80;
@@ -13,41 +13,50 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
-  playPause (song = this.currentlyPlaying) {
+
+  playPause(song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
       this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
     }
     if (this.playState === 'paused' || this.playState === 'stopped') {
-      this.soundObject.setVolume( this.volume );
+      this.soundObject.setVolume(this.volume);
+      //Play the song that is paused or stopped
       this.soundObject.play();
+      //show playing
       this.playState = 'playing';
+      //if song is paused or stop, return "playing"
       this.currentlyPlaying.element.removeClass('paused').addClass('playing');
     } else {
+      //Otherwise, pause song
       this.soundObject.pause();
       this.playState = 'paused';
+      //Clear classes on the song that's playing and add "paused"
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
-  skipTo (percent) {
-    if (this.playState !== 'playing') { return }
-    this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
+
+  skipTo(percent) {
+    //update our playstate properties
+    if (this.playState !== 'playing') {
+      return
+    }
+    //return with duration percentage
+    this.soundObject.setTime((percent / 100) * this.soundObject.getDuration());
   }
-  
-  setVolume (percent) {
+
+  setVolume(percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
   }
 }
 
-const player = new Player();
 
+const player = new Player();
