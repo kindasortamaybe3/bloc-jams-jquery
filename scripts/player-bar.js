48d70f0698bc1940d10291cb1f7159cfd9b76107
Kara Bruce <kindasortamaybe3@gmail.com>
2019-12-05 17:36:39 +0100

    fixing files - deleted player.js by mistake

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,35 +1,37 @@
 {
-  $("button#play-pause").on("click", function() {
-    player.playPause();
-    $(this).attr("playState", player.playState);
-  });
+    $('button#play-pause').on('click', function () {
+        player.playPause();
+        $(this).attr('playState', player.playState);
+    });
 
-  $("button#next").on("click", function() {
-    if (player.playState !== "playing") {
-      return;
-    }
+    $('button#next').on('click', function () {
+        if (player.playState !== 'playing') {
+            return;
+        }
 
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) {
-      return;
-    }
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const nextSongIndex = currentSongIndex + 1;
+        if (nextSongIndex >= album.songs.length) {
+            return;
+        }
 
-  $("#time-control .current-time").text(currentTime);
-  $("#time-control input").on("input", function(event) {
-    player.skipTo(event.target.value);
-  });
+        const nextSong = album.songs[nextSongIndex];
+        player.playPause(nextSong);
+    });
 
-  setInterval(() => {
-    if (player.playState !== "playing") {
-      return;
-    }
-    const currentTime = player.getTime();
-    const duration = player.getDuration();
-    const percent = (currentTime / duration) * 100;
-    $("#time-control input").val(precent);
-  }, 1000);
+    $('button#previous').on('click', function () {
+        if (player.playState !== 'playing') {
+            return;
+        }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const previousSongIndex = currentSongIndex - 1;
+        if (previousSongIndex >= album.songs.length) {
+            return;
+        }
+
+        const previousSong = album.songs[previousSongIndex];
+        player.playPause(previousSong);
+    });
+
 }
