7c111b09fa9bc0d6dc032a6e39b9295d8634a141
Kara Bruce <kindasortamaybe3@gmail.com>
2019-12-05 16:44:13 +0100

    current time and playback added

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,20 +1,35 @@
 {
-    $('button#play-pause').on('click', function () {
-        player.playPause();
-        $(this).attr('playState', player.playState);
-    });
+  $("button#play-pause").on("click", function() {
+    player.playPause();
+    $(this).attr("playState", player.playState);
+  });
 
-    $('button#next').on('click', function () {
-        if (player.playState !== 'playing') {
-            return;
-        }
+  $("button#next").on("click", function() {
+    if (player.playState !== "playing") {
+      return;
+    }
 
-        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-        const nextSongIndex = currentSongIndex + 1;
-        if (nextSongIndex >= album.songs.length) {
-            return;
-        }
-        const nextSong = album.songs[nextSongIndex];
-        player.playPause(nextSong);
-    });
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) {
+      return;
+    }
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
+  $("#time-control .current-time").text(currentTime);
+  $("#time-control input").on("input", function(event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval(() => {
+    if (player.playState !== "playing") {
+      return;
+    }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $("#time-control input").val(precent);
+  }, 1000);
 }
